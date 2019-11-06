d5b9be8bffa985f7198537601e44a31960c1ded1
lanjaifeng <lanjiafeng@nnmtl.com>
2019-11-06 16:17:21 +0800

    sdwda

--- pages/promotional-items/index.js
+++ pages/promotional-items/index.js
@@ -11,7 +11,13 @@ Page({
       'navbar': '1',
       'return': '1',
       'title': '促销单品'
-    }
+    },
+    imgUrls: [],
+    bastList: [],
+    name: '',
+    icon: '',
+    type: 4,
+    status: 0,
   },
 
   /**
@@ -38,9 +44,26 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var type = this.data.type;
+    if (type == 1) {
+      this.setData({ 'parameter.title': '精品推荐', name: '精品推荐', icon: 'icon-jingpintuijian' });
+    } else if (type == 2) {
+      this.setData({ 'parameter.title': '热门榜单', name: '热门榜单', icon: 'icon-remen', status: 1 });
+    } else if (type == 3) {
+      this.setData({ 'parameter.title': '首发新品', name: '首发新品', icon: 'icon-xinpin' });
+    } else if (type == 4) {
+      this.setData({ 'parameter.title': '促销单品', name: '促销单品', icon: 'icon-cuxiaoguanli' });
+    } else {
+      this.setData({ 'parameter.title': '首发新品', name: '首发新品', icon: 'icon-xinpin' });
+    }
+    this.getIndexGroomList();
   },
-
+  getIndexGroomList: function () {
+    var that = this;
+    getGroomList(that.data.type).then(res => {
+      that.setData({ imgUrls: res.data.banner, bastList: res.data.list })
+    });
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
